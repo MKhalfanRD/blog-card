f93acf88405811e5cefad283e6e709a7d9d940d1
MKhalfanRD <khalfanrd2002@gmail.com>
2024-07-27 16:51:17 +0700

    blog card by buba

---
+++ tailwind.config.js
@@ -0,0 +1,21 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {
+      colors: {
+        "ungu-muda": '#E6D6FC',
+        "ungu-tua": '#883AE1',
+        "abu-muda": '#E5EAF0',
+        "abu-tua": '#6C727F'
+      },
+      backgroundColor: {
+        "putih-gelap": 'FAFAF9' 
+      },
+      boxShadow: {
+        "shadow": "0px 30px 100px 0px rgba(17, 23, 41, 0.05)"
+      },
+    },
+  },
+  plugins: [],
+}
